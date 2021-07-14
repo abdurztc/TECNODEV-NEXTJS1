5213a5a4c815c54426e3ec06aa8c7acf5063410e
abdurztc <abdurztc@gmail.com>
2021-07-14 21:41:47 +0700

    Setup Auto Redirection 404 Page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,13 @@
+import { useEffect } from "react"
+import { useRouter } from "next/dist/client/router";
 
 export default function Custom404() {
+    const router = useRouter();
+    useEffect(() => {
+        setTimeout(() => {
+           router.push('/');
+        },5000);
+    },[]);
     return (
         <div>
            <h1 className="title-notfound">Oooops....</h1>
