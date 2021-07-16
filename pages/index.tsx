a58f59902ee8e111c96924be58b888b80185ea35
abdurztc <abdurztc@gmail.com>
2021-07-16 21:59:17 +0700

    Image Optimalisation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,54 @@
 import Layout from "../component/Layout";
 import styles from "../styles/Home.module.css";
 import Head from 'next/head';
+import Image from 'next/image';
 export default function Home() {
   return (
     <Layout pageTitle="Home Page" >
+          <Image src="/dasha.jpg" width={200} height={200} alt="dasha-profile" />
+          {/* <img src="/dasha.jpg" style={{width: 200, height: 200}} alt="dasha-profile img" /> */}
           <h1 className={styles['title-homepage']}>Welcome TECHNODEV</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <Image src="/elina.jpg" width={200} height={200} alt="elina-profile" />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora neque maiores! Reiciendis voluptatem voluptate dolorum, eligendi earum sequi. Veniam, repellendus porro eveniet voluptates eligendi veritatis iure pariatur qui autem.</p>
+          <Image src="/bora.png" width={200} height={200} alt="bora-profile" />
     </Layout>
   );
 }
